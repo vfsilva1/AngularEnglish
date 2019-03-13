056201bb59bfbed086fe6641f25059c07ca1332e
Vitor Ferreira da Silva <vfsilva5@gmail.com>
2019-03-13 09:12:43 -0300

    Tentativas

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -10,17 +10,17 @@ import { Frases } from './frases-mock';
 export class PainelComponent implements OnInit {
   public frases: Frase[] = Frases;
   public instrucao: string = 'Traduza a frase:'
-  public resposta: string
+  public resposta: string = ''
 
   public rodada: number = 0
   public rodadaFrase: Frase
 
   public progresso: number = 0
 
-  constructor() { 
-    this.rodadaFrase = this.frases[this.rodada] 
-    console.log(this.rodadaFrase)
+  public tentativas: number = 3
 
+  constructor() { 
+    this.atualizaRodada()
   } 
 
   ngOnInit() {
@@ -28,19 +28,30 @@ export class PainelComponent implements OnInit {
 
   public atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value
-    console.log(this.resposta)
   }
 
   public verificarResposta(): void {
     if (this.resposta == this.rodadaFrase.frasePtBr) { 
-      alert('Tradução correta!')
-      this.rodadaFrase = this.frases[++this.rodada]
+      ++this.rodada
+      this.atualizaRodada()
 
       this.progresso = this.progresso + (100 / this.frases.length)    
-      console.log(this.progresso)
+
+      if (this.rodada == 4) {
+        alert('Você concluiu as traduções com sucesso!')
+      }
     }
     else {
-      alert('Tradução incorreta!')
+      this.tentativas--
+      if (this.tentativas == -1)
+        alert('Você perdeu todas as tentaivas!')
+      else
+        alert('Tradução incorreta!')
     }
+  }
+
+  public atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada]
+    this.resposta = ''
   }
 }
--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { Coracao } from '../shared/coracao.model';
 
 @Component({
   selector: 'app-tentativas',
@@ -6,11 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tentativas.component.css']
 })
 export class TentativasComponent implements OnInit {
-
   public coracaoVazio: string = '/assets/coracao_vazio.png'
   public coracaoCheio: string = '/assets/coracao_cheio.png'
 
+  @Input() public tentativas: number
+
+  public coracoes: Array<Coracao> = [
+    new Coracao(true), new Coracao(true), new Coracao(true)
+  ]
+
   constructor() { }
+
+  ngOnChanges() {
+    if (this.tentativas != this.coracoes.length) {
+      let indice = this.coracoes.length - this.tentativas
+
+      this.coracoes[indice - 1].cheio = false
+    }
+  }
 
   ngOnInit() {
   }
